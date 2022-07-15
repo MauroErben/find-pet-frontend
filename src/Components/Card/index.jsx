08bd14e3611049d70c8e66faa75e61a45c764da5
Mauro Erben <mauroerben100@hotmail.com>
2022-07-15 07:55:38 -0300

    fix skeleton card

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,46 +1,41 @@
 import React from 'react'
-import { VStack, Divider, Text, SimpleGrid } from '@chakra-ui/react'
+import { VStack, Divider, SimpleGrid } from '@chakra-ui/react'
 import CardHeader from './CardHeader'
 import CardBody from './CardBody'
 import CardFooter from './CardFooter'
 import CardSkeleton from '../Skeleton'
 
 export default function CardPost({ postData, loading }) {
-    if (postData.length > 0) {
-        return (
-            <SimpleGrid p={[4, 4, 6, 8]} columns={{ sm: 1, md: 2, lg: 3 }} spacing={8}>
-                {postData.map(item => (
-                    <VStack
-                        justifyContent='center'
-                        alignItems='center'
-                        borderWidth='1px'
-                        shadow='md'
-                        key={item.id}
-                        p={4}
-                    >
-                        <CardSkeleton isLoaded={loading} >
-                            <CardHeader image={item.image} title={item.title} />
-                        </CardSkeleton>
-
-                        <CardSkeleton isLoaded={loading} >
-                            <CardBody
-                                body={item.description}
-                                time={item.createdAt}
-                                location={item.location}
-                            />
-                        </CardSkeleton>
-
-                        <Divider />
-
-                        <CardSkeleton isLoaded={loading} >
-                            <CardFooter post={item} />
-                        </CardSkeleton>
-                    </VStack>
-                ))}
-            </SimpleGrid>
-        )
-    }
     return (
-        <Text>Loading...</Text>
+        <SimpleGrid p={[4, 4, 6, 8]} columns={{ sm: 1, md: 2, lg: 3 }} spacing={8}>
+            {postData.map(item => (
+                <VStack
+                    justifyContent='center'
+                    alignItems='center'
+                    borderWidth='1px'
+                    shadow='md'
+                    key={item.id}
+                    p={4}
+                >
+                    <CardSkeleton isLoaded={loading} >
+                        <CardHeader image={item.image} title={item.title} />
+                    </CardSkeleton>
+
+                    <CardSkeleton isLoaded={loading} >
+                        <CardBody
+                            body={item.description}
+                            time={item.createdAt}
+                            location={item.location}
+                        />
+                    </CardSkeleton>
+
+                    <Divider />
+
+                    <CardSkeleton isLoaded={loading} >
+                        <CardFooter post={item} />
+                    </CardSkeleton>
+                </VStack>
+            ))}
+        </SimpleGrid>
     )
 }
--- src/Components/Hooks/post.jsx
+++ src/Components/Hooks/post.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 export default function usePost() {
     const [result, setResult] = useState({
-        data: [],
+        data: [{}],
         isLoaded: false
     })
 
