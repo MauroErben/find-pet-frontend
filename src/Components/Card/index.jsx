d0ce7c26e67b05bb84cc2f4268f1ee417918a2b3
Mauro Erben <mauroerben100@hotmail.com>
2022-07-19 21:22:59 -0300

    Delete card skeleton

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,9 +3,8 @@ import { VStack, Divider, SimpleGrid } from '@chakra-ui/react'
 import CardHeader from './CardHeader'
 import CardBody from './CardBody'
 import CardFooter from './CardFooter'
-import CardSkeleton from '../Skeleton'
 
-export default function CardPost({ postData, loading }) {
+export default function CardPost({ postData }) {
     return (
         <SimpleGrid p={[4, 4, 6, 8]} columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={8}>
             {postData.map((item, index) => (
@@ -17,23 +16,17 @@ export default function CardPost({ postData, loading }) {
                     shadow='md'
                     p={4}
                 >
-                    <CardSkeleton isLoaded={loading} >
-                        <CardHeader image={item.image} title={item.title} />
-                    </CardSkeleton>
+                    <CardHeader image={item.image} title={item.title} />
 
-                    <CardSkeleton isLoaded={loading} >
-                        <CardBody
-                            body={item.description}
-                            time={item.createdAt}
-                            location={item.location}
-                        />
-                    </CardSkeleton>
+                    <CardBody
+                        body={item.description}
+                        time={item.createdAt}
+                        location={item.location}
+                    />
 
                     <Divider />
 
-                    <CardSkeleton isLoaded={loading} >
-                        <CardFooter post={item} />
-                    </CardSkeleton>
+                    <CardFooter post={item} />
                 </VStack>
             ))}
         </SimpleGrid>
