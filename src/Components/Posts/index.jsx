243020ea06df16f7093b76280846748fae5bc566
Mauro Erben <mauroerben100@hotmail.com>
2022-07-19 21:22:37 -0300

    changed fetching of post

--- src/Components/Posts/index.jsx
+++ src/Components/Posts/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CardPost from '../Card'
-import usePost from '../../Hooks/post'
+import { getAllPost } from '../../Services/Api'
 
 export default function Posts () {
-    // Get all post
-    const { data, isLoaded } = usePost('http://localhost:3001/posts')
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const allPost = await getAllPost()
+            setData(allPost) 
+        }
+        getPosts()
+    }, [])
+
     return(
-        <CardPost postData={data} loading={isLoaded} />
+        <CardPost postData={data} />
     )
 }
